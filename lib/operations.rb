39bd21a6f99ab50a59812c16a686434555465183
Juan Tenezaca <tenj34@gmail.com>
2018-06-30 15:49:42 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
+  if speed > 60
+    return true
+  elsif speed < 40
+    return true
+  elsif speed >= 40 && speed >= 60
+    return false
+
 
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
